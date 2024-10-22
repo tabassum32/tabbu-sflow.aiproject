bad6288e0e7e1ab5604cbe3f5b189362ccaccab7
tabassum32 <tabassumbanu32@gmail.com>
2024-10-23 00:30:00 +0530

    first commit

---
+++ myapp/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const mongooseURL = process.env.MONGO_URI;
+const db = async () => {
+ try {
+ await mongoose.connect(mongooseURL);
+ console.log("DB Connected..");
+ } catch (e) {
+ console.log(e);
+ }
+};
+
+db(mongoose.connection);
+module.exports = db;
+
